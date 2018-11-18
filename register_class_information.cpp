ba2a851f9361474c54ca6bd620f2f89aa8b46f73
ngzHappy <zhaixueqiang@hotmail.com>
2018-11-18 16:14:42 +0800

    ???

---
+++ register_class_information.cpp
@@ -0,0 +1,90 @@
+#include "register_class_information.hpp"
+#include <unordered_map>
+#include <shared_mutex>
+
+namespace _18_11_18_private {
+
+    namespace {
+        namespace this_cpp_file {
+
+            class type_index_hash {
+            public:
+                std::size_t operator()(const ExportRegisterClassInformation::type_index & arg) const {
+                    return arg.first;
+                }
+            };
+
+            class class_information {
+            public:
+                ExportRegisterClassInformation::static_cast_map up_cast_map;
+                class_information() {
+                }
+                class_information(ExportRegisterClassInformation::static_cast_map && arg) :
+                    up_cast_map(std::move(arg)) {
+                }
+            };
+
+            class type_index_hash_map {
+                using hash_map = std::unordered_map<
+                    ExportRegisterClassInformation::type_index,
+                    class_information,
+                    type_index_hash
+                >;
+                hash_map mmm_hash_map;
+                std::shared_mutex mmm_mutex;
+            public:
+                const class_information * insert(
+                    const ExportRegisterClassInformation::type_index & key,
+                    ExportRegisterClassInformation::static_cast_map && up_cast_map) {
+                        {/*try to find ...*/
+                            std::shared_lock varReadLock{ mmm_mutex };
+                            auto varPos = std::as_const(mmm_hash_map).find(key);
+                            if (varPos != mmm_hash_map.end()) {
+                                return &(varPos->second);
+                            }
+                        }
+                        std::unique_lock varWriteLock{ mmm_mutex };
+                        {/*try to find again ...*/
+                            auto varPos = std::as_const(mmm_hash_map).find(key);
+                            if (varPos != mmm_hash_map.end()) {
+                                return &(varPos->second);
+                            }
+                        }
+                        /*insert the data ...*/
+                        auto varPos = mmm_hash_map.emplace(key, std::move(up_cast_map));
+                        return &(varPos.first->second);
+                }
+            };
+
+            static inline type_index_hash_map * get_type_index_hash_map() {
+                /*never delete ...*/
+                static type_index_hash_map * varAns{ new type_index_hash_map };
+                return varAns;
+            }
+
+        }/**/
+    }/**/
+
+    const ExportRegisterClassInformation::static_cast_map * ExportRegisterClassInformation::register_up_cast_map(
+        const type_index & argIndex,
+        static_cast_map && argMap) {
+        auto varMap = this_cpp_file::get_type_index_hash_map();
+        return &(varMap->insert(argIndex, std::move(argMap))->up_cast_map);
+    }
+
+}/*namespace _18_11_18_private*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ register_class_information.hpp
@@ -0,0 +1,126 @@
+#pragma once
+
+#include <map>
+#include <utility>
+#include <typeinfo>
+#include <typeindex>
+#include <functional>
+#include <type_traits>
+
+namespace _18_11_18_private {
+
+    template<typename K, typename T>
+    using map = std::map<K, T>;
+
+    class ExportRegisterClassInformation {
+    public:
+        using type_index = std::pair<std::size_t, std::type_index>;
+        typedef void*(*static_type_cast_function)(void *);
+        using static_cast_map = map<type_index, static_type_cast_function>;
+        static const static_cast_map * register_up_cast_map(const type_index &, static_cast_map &&);
+    protected:
+        template< template<typename ...> class I, typename ... T >
+        inline static std::size_t ppp_class_size(const I<T ...> &) {
+            return (sizeof...(T));
+        }
+        template<typename U>
+        inline static type_index ppp_get_type_index() {
+            using this_class_type_ = std::remove_cv_t< std::remove_reference_t<U> >;
+            typename type_index::second_type varIndex{ typeid(this_class_type_) };
+            std::hash<typename type_index::second_type> varIndexHash;
+            return type_index{ varIndexHash(varIndex) ,varIndex };
+        }
+        template< typename U, template<typename ...> class I, typename ... T >
+        inline static static_cast_map * ppp_create_class_up_cast(static_cast_map * map, const I<T ...> &) {
+            using this_class_type_ = std::remove_cv_t< std::remove_reference_t<U> >;
+            ((void)(map->emplace(ppp_get_type_index<T>(),
+                [](void * arg)->void * { return static_cast<std::remove_cv_t<
+                std::remove_reference_t< T > > *>(
+                    reinterpret_cast<this_class_type_ *>(arg)); })), ...);
+            return map;
+        }
+    };
+
+    template<typename T>
+    class RegisterClassInformation : public ExportRegisterClassInformation {
+    public:
+        using type_index = ExportRegisterClassInformation::type_index;
+        using static_cast_map = ExportRegisterClassInformation::static_cast_map;
+        static const int class_deeth/*继承树的深度*/;
+        static const type_index class_index/*当前类的type_index*/;
+        static const static_cast_map * class_up_cast_map/*向上类型转换表*/;
+    private:
+        inline static int register_class_depth();
+        inline static type_index register_class_index();
+        inline static const static_cast_map * register_static_up_cast_map();
+    };
+
+    template<typename T>
+    const int RegisterClassInformation<T>::class_deeth =
+        RegisterClassInformation<T>::register_class_depth();
+
+    template<typename T>
+    const typename RegisterClassInformation<T>::type_index RegisterClassInformation<T>::class_index =
+        RegisterClassInformation<T>::register_class_index();
+
+    template<typename T>
+    const typename RegisterClassInformation<T>::static_cast_map * RegisterClassInformation<T>::class_up_cast_map =
+        RegisterClassInformation<T>::register_static_up_cast_map();
+
+    template<typename T>
+    inline int RegisterClassInformation<T>::register_class_depth() {
+        using this_class_type_ = std::remove_cv_t< std::remove_reference_t<T> >;
+        return ppp_class_size<>();
+    }
+
+    template<typename T>
+    inline const typename RegisterClassInformation<T>::static_cast_map * RegisterClassInformation<T>::register_static_up_cast_map() {
+        using this_class_type_ = std::remove_cv_t< std::remove_reference_t<T> >;
+        static_cast_map varUpCastMap;
+        ppp_create_class_up_cast<this_class_type_>;
+        return RegisterClassInformation::register_up_cast_map(
+            RegisterClassInformation::register_class_index(),
+            std::move(varUpCastMap));
+    }
+
+    template<typename T>
+    inline typename RegisterClassInformation<T>::type_index RegisterClassInformation<T>::register_class_index() {
+        using this_class_type_ = std::remove_cv_t< std::remove_reference_t<T> >;
+        return ppp_get_type_index< this_class_type_ >();
+    }
+
+}/*namespace _18_11_18_private*/
+
+template<typename T>
+class RegisterClassInformation : private _18_11_18_private::RegisterClassInformation<
+    std::remove_cv_t< std::remove_reference_t<T> > > {
+    using super_type = _18_11_18_private::RegisterClassInformation<
+        std::remove_cv_t< std::remove_reference_t<T> > >;
+public:
+    using type_index = typename super_type::type_index;
+    using type_up_cast_map = typename super_type::static_cast_map;
+    static inline const int & get_class_deeth() {
+        return super_type::class_deeth;
+    }
+    static inline const type_index & get_class_index() {
+        return super_type::class_index;
+    }
+    static inline const type_up_cast_map & get_class_up_cast_map() {
+        return *(super_type::class_up_cast_map);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
